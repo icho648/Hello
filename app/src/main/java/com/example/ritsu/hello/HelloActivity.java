d8e204aace0eb5a6d30226c778740275be7de0c0
icho648 <ritsu2015@foxmail.com>
2018-09-13 09:29:37 +0800

    test4

--- app/src/main/java/com/example/ritsu/hello/HelloActivity.java
+++ app/src/main/java/com/example/ritsu/hello/HelloActivity.java
@@ -2,6 +2,7 @@ package com.example.ritsu.hello;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class HelloActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class HelloActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+        Log.d("test","hello world");
     }
 }
