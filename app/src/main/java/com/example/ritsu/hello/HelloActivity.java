e3d59c890902e8c95833021ee1944f6f3f6ada7c
icho648 <ritsu2015@foxmail.com>
2018-09-13 09:35:46 +0800

    test4

--- app/src/main/java/com/example/ritsu/hello/HelloActivity.java
+++ app/src/main/java/com/example/ritsu/hello/HelloActivity.java
@@ -10,6 +10,5 @@ public class HelloActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
-        Log.d("test","hello world");
     }
 }
